c4bd904f09fdabc792c3b30ad92af761958d517c
Adrian Darian <adarian@ucmerced.edu>
2018-12-30 22:46:17 -0500

    Add Blazor App

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BitSite {
+    public class Startup {
+        public void ConfigureServices (IServiceCollection services) { }
+
+        public void Configure (IBlazorApplicationBuilder app) {
+            app.AddComponent<App> ("app");
+        }
+    }
+}
